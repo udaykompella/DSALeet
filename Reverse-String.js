1021d9746bd3720813faece747cce1723625f473
Kompella Uday Mani Kumar <kompellauday9@gmail.com>
2025-07-15 22:35:40 +0530

    [344] [Time Beats: 54.65%] [Memory Beats: 99.53%] - LeetPush

--- Reverse-String.js
+++ Reverse-String.js
@@ -7,8 +7,8 @@ var reverseString = function(s) {
     let halflen = Math.floor(len / 2)
     for(let i = 0; i< halflen;i++){
        temp = s[i]
-       s[i] = s[len-i-1]
-       s[len-i-1] = temp
+       s[i] = s[len-1-i]
+       s[len-1-i] = temp
     }
     return s
 };
