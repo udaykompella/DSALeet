207753dc0510706fea891cc771a5d904d974a201
Kompella Uday Mani Kumar <kompellauday9@gmail.com>
2026-06-10 17:10:03 +0530

    Time: 9 ms (12.63%), Space: 71.7 MB (5.12%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let avg = 0
+    let low = 0
+    let high = k - 1
+    let sum = 0
+    // let res = 0
+    for(let i = low;i<=high;i++){
+        sum = sum + nums[i]
+    }
+    avg = calculateAverage(high,low,sum)
+    let res = avg
+    while(high < nums.length){
+        res = Math.max(avg, res)
+        low++
+        high++
+        if(high === nums.length){
+            break
+        }
+        sum = sum - nums[low - 1]
+        sum = sum + nums[high]
+        avg = calculateAverage(high,low,sum)
+    }
+    return res
+};
+
+function calculateAverage(high,low,sum){
+    let len = high - low + 1
+    avg = sum / len
+    return avg
+}
